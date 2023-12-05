96ae28fff89874fa43c434b6c48f9a68bbfb7a18
CANO <umutcanbozyigit34012@gmail.com>
2023-12-06 00:03:48 +0300

    get-set

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    public String name;
+    public int pageNumber;
+
+    public Book( String name,int pageNumber){
+        this.name=name;
+        if(pageNumber<0){
+            pageNumber=0;
+
+        }
+        this.pageNumber=pageNumber;
+
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Book b1=new Book("harry",45);
+        Book b2=new Book("lord",55);
+        b1.setName("hary");
+        System.out.println(b2.getPageNumber());
+
+
+    }
+}
